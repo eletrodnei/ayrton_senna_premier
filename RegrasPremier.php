0c0678f56a36fa7c6c4e148628d734ea3ff21821
Rodnei Miranda da Silva <eletrodnei@hotmail.com>
2020-02-20 11:33:26 -0300

    primeiro envio

---
+++ RegrasPremier.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+         <link href="styles/style.css" rel="stylesheet"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="corporegras">
+            <div class="transbox">
+                <big>
+                    <b>
+                        <h1 class="tituloregras">REGRAS DO CAMPEONATO<br></h1>
+            
+<br>
+Serão 2 divisões, transmitidas ao vivo por youtube e ou facebook.<br>
+<br>
+1 - os primeiros vao ficando com carros piores com relação a termino na corrida<br>
+2 - pontos conforme f1 normal <br>
+2.5 - na primeira corrida o desempenho será igual para todos e da segunda até a sexta será igual item 1<br>
+3- na ultima corrida serão 2 corridas de 25% a primeira normal com qualificação conforme item 1 e a segunda grid invertido conforme tabela do campeonato desempenho igual<br> 
+4- transmissões fica definida por kiuki e seven quem faz 16hr e 18hrs, será ao vivo por youtube e ou facebook.<br>
+5- pilotos reserva não pontuará para o titular, poderam jogar na outra divisão que não são titulares<br>
+6 - os 4 primeiros da divisão 2 irá subir para divisão na próxima temporada<br>
+7 - os 4 ultimos da divisão 1 será rebaixado para a divisão 2<br>
+8 – Combate a uso de Hack e outros : Os cinco primeiros deverão fazer verificação de arquivos e enviar para o email - verificacao.aspl@gmail.com<br>
+<br> 
+<br>
+Formato da Liga<br>
+Carros 2019<br>
+Treino 18 min.<br>
+Corrida 50%<br>
+Desempenho Realista<br>
+Dano Veículo regular<br>
+Clima Dinamico<br>
+Parque Fechado <br>
+Corte Curva Rígido <br>
+Setup aberto<br>
+<br>
+<br>
+1º divisão - todos domingos as 18hrs<br>
+<br>
+flavio - edplay - willians<br>
+rodnei – flexx - toro roso<br>
+kuliuk - buka - alfa romeo<br>
+skral  - gelson - racing point<br>
+maluquinho - vovo jorge - mclaren<br>
+pedrovisk - claudio_mda - has<br>
+gara19 - smith - renalt<br>
+porman - Celso brito jr  - red bull<br>
+villar - bruno senna - ferrari<br>
+bozo - jonny - mercedes<br>
+<br>
+<br>
+2º divisão todos domingos as 16hrs<br>
+<br>
+grimaldix - bnz - ferrari<br>
+marcio-abdu - rlugui - tororoso<br>
+francinaldo- Roobens - alfa romeo<br>
+felix - rodox - racing point<br>
+ - ariassu – mclaren<br>
+punta cana - renault<br>
+leo - gargio- has <br>
+Polux - redbull <br>
+wess - willians<br>
+<br>
+<br>
+Pistas na primeira temporada<br>
+<br>
+1 barein<br>
+2 Espanha<br>
+3 Italia<br>
+4 Monaco<br>
+5 Spa<br>
+6 Japao<br>
+7 Brasil grande final<br></big></b>
+            </div></div>
+        <?php
+        
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ StartPremier.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+ 
+        <link href="styles/style.css" rel="stylesheet"/>
+        <meta charset="utf-8"/>
+        <title></title>
+    </head>
+    <body> 
+         
+         
+        <div class="corpo">
+    
+        <div><h1 class="h1"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+           AYRTON SENNA PREMIER LEAGUE</h1> 
+        <div class="botao"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+               <input type="button" value="Cadastrar"  name="cadastro" /> 
+               <input type="button" value="Regras"     onclick="window.location='RegrasPremier.php'" 
+                      name="regras" /> 
+        </div>
+        </div>
+    <?php
+        
+    ?>    
+        
+        </div>
+    </body>
+</html>
+
+
